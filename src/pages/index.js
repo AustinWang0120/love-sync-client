c27d4febd921b1a78f62eb1ca2fad1fe8356c25e
Austin Wang <yyaustin@bu.edu>
2023-11-14 23:44:38 -0500

    Add API calls to fetch shared content and messages

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,4 +9,35 @@ export default function Home() {
 	// 用于存储共享内容和消息的状态
 	const [sharedContent, setSharedContent] = useState([]);
 	const [messages, setMessages] = useState([]);
+
+	// 从模拟的API获取共享内容和消息
+	useEffect(() => {
+		// 从模拟的API获取共享内容
+		const fetchSharedContent = async () => {
+			const response = await fetch(
+				"http://localhost:3001/shared?coupleId=" + coupleId
+			);
+			const data = await response.json();
+			setSharedContent(data[0].posts);
+		};
+
+		// 从模拟的API获取消息
+		const fetchMessages = async () => {
+			const response = await fetch(
+				"http://localhost:3001/messages?coupleId=" + coupleId
+			);
+			const data = await response.json();
+			setMessages(data[0].chat);
+		};
+
+		fetchSharedContent();
+		fetchMessages();
+	}, [coupleId]);
+
+	return (
+		<div>
+			<SharedSpace sharedContent={sharedContent} />
+			<Chat messages={messages} currentUserId={currentUserId} />
+		</div>
+	);
 }
